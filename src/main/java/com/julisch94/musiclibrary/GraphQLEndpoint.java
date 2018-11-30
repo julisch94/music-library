b40fde7b168968432045ff2287b0396dec0d72a8
Julian Schmidt <julisch94@gmx.net>
2018-11-30 23:19:21 +0100

    add serialVersionUID

--- src/main/java/com/julisch94/musiclibrary/GraphQLEndpoint.java
+++ src/main/java/com/julisch94/musiclibrary/GraphQLEndpoint.java
@@ -7,6 +7,8 @@ import graphql.servlet.SimpleGraphQLServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
+    private static final long serialVersionUID = 1L;
+
     public GraphQLEndpoint() {
         super(SchemaParser.newParser().file("schema.graphqls").build().makeExecutableSchema());
     }
