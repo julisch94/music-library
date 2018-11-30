06a4ef48fb422e83acae95255e6e656e5cdf1331
Julian Schmidt <julisch94@gmx.net>
2018-12-01 00:07:24 +0100

    add resolver to schema

--- src/main/java/com/julisch94/musiclibrary/GraphQLEndpoint.java
+++ src/main/java/com/julisch94/musiclibrary/GraphQLEndpoint.java
@@ -2,6 +2,8 @@ package com.julisch94.musiclibrary;
 
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
+
+import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
 @WebServlet(urlPatterns = "/graphql")
@@ -10,6 +12,14 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
     private static final long serialVersionUID = 1L;
 
     public GraphQLEndpoint() {
-        super(SchemaParser.newParser().file("schema.graphqls").build().makeExecutableSchema());
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        Library library = new Library();
+        library.addArtist(new Artist("Ed Sheeran"));
+        library.addArtist(new Artist("Outbreakband"));
+        return SchemaParser.newParser().file("schema.graphqls").resolvers(new Query(library)).build()
+                .makeExecutableSchema();
     }
 }
